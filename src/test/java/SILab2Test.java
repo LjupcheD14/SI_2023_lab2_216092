82a73e9ace270585f997152dfd5be1d4485b4e75
Ljubomir <ljupcedavitkov14@gmail.com>
2023-05-31 13:01:30 +0200

    Gradle

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,19 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+
+    @Test
+    void testMultipleConditionCriterion() {
+        // Write test cases for the Multiple Condition criterion
+        // Use assert statements to validate the expected results
+    }
+
+    @Test
+    void testEveryBranchCriterion() {
+        // Write test cases for the Every Branch criterion
+        // Use assert statements to validate the expected results
+    }
+}
